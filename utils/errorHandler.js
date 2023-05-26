676906cc0cdc3095e3ae93805cbcaa1710b6a9fa
NaikyDing <74ding@gmail.com>
2023-05-26 17:26:57 +0800

    feat(utils): errorHandler 抽離 express-validator 驗證

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,20 +1,12 @@
 const { errorResponse } = require('../utils/responseHandlers')
-const { validationResult } = require('express-validator')
 
 const errorCallback = ({ req, res, next, errors }) => {
   console.log('----------- 🚧 errorCallback 🚧 -----------')
   console.log(errors)
   console.log('----------- 🚧 errorCallback 🚧 -----------')
 
-  // express-validator 驗證
-  const errorsValidate = validationResult(req)
-    // 指定回傳欄位
-    .formatWith((errors) => errors.msg)
-    .array()
-  if (errorsValidate.length > 0)
-    return errorResponse({ res, errors: errorsValidate, statusCode: 400 })
-
   const errorsIsExist = errorDefault[errors.name]
+
   const errorsData =
     errors.name === 'ValidationError'
       ? Object.values(errors.errors).map((item) => item.properties.message)
