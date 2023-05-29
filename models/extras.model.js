455fe002cba0a0ba5ec8e4703c4e68902a6b9566
Naiky <74ding@gmail.com>
2023-05-29 17:25:01 +0800

    feat(model): extras

--- models/extras.model.js
+++ models/extras.model.js
@@ -1,30 +1,39 @@
 const mongoose = require('mongoose')
 
-const extrasSchema = new mongoose.Schema({
-  type: {
-    required: [true, 'type 是必填項目'],
-    type: String,
+const extrasSchema = new mongoose.Schema(
+  {
+    type: {
+      type: String,
+      required: [true, 'type 是必填項目'],
+    },
+    name: {
+      type: String,
+      required: [true, 'name 是必填項目'],
+    },
+    description: {
+      type: String,
+      required: [true, 'description 是必填項目'],
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+    price: {
+      type: Number,
+      required: [true, 'price 是必填項目'],
+    },
+    agents: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, 'agents 是必填項目'],
+        ref: 'Agent',
+      },
+    ],
   },
-  name: {
-    required: [true, 'name 是必填項目'],
-    type: String,
-  },
-  description: {
-    required: [true, 'description 是必填項目'],
-    type: String,
-  },
-  image: {
-    required: [true, 'name 是必填項目'],
-    type: String,
-  },
-  price: {
-    required: [true, 'price 是必填項目'],
-    type: Number,
-  },
-  agents: {
-    required: [true, 'agents 是必填項目'],
-    type: Number,
-  },
-})
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+)
 
 module.exports = mongoose.model('Extra', extrasSchema)
