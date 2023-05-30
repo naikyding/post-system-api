6fedcfa3309dac5882a23ee149c8abac2ad0a206
Naiky <74ding@gmail.com>
2023-05-30 22:45:01 +0800

    feat(model): custromers

--- models/customers.model.js
+++ models/customers.model.js
@@ -1,1 +1,64 @@
+const mongoose = require('mongoose')
 
+const customerSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, '`name`必填'],
+    },
+    description: {
+      type: String,
+      required: [true, '`description`必填'],
+    },
+    gender: {
+      type: String,
+      enum: ['male', 'female', 'non-binary'],
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default: '',
+    },
+    email: {
+      type: String,
+      required: [true, '`email`必填'],
+    },
+    phone: {
+      type: String,
+      default: '',
+    },
+    address: {
+      type: String,
+      default: '',
+    },
+    googleId: {
+      type: String,
+      default: '',
+    },
+    facebookId: {
+      type: String,
+      default: '',
+    },
+    password: {
+      type: String,
+      required: [true, '`password`必填'],
+    },
+    note: {
+      type: String,
+      default: '',
+    },
+    agents: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, 'agents 是必填項目'],
+        ref: 'Agent',
+      },
+    ],
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('Customer', customerSchema)
