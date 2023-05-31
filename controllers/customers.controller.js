04a0596c893ba4ddaef853770750a8b21b3655d7
Naiky <74ding@gmail.com>
2023-05-31 13:46:32 +0800

    feat(controller): create customer validation

--- controllers/customers.controller.js
+++ controllers/customers.controller.js
@@ -1,7 +1,72 @@
 const catchAsync = require('../utils/catchAsync')
 
+const { body } = require('express-validator')
+const agentsModel = require('../models/agents.model')
+
 const validation = {
-  createCustomer: [],
+  createCustomer: [
+    body('name')
+      .exists() // 欄位存在
+      .withMessage('欄位 `name` 必填')
+      .bail() // 不可為空
+      .notEmpty()
+      .withMessage('`name` 不可為空值')
+      .bail()
+      .isString() // 為字串格式
+      .withMessage('`name` 必須為字串格式'),
+
+    body('gender')
+      .exists() // 欄位存在
+      .withMessage('欄位 `gender` 必填')
+      .bail() // 不可為空
+      .notEmpty()
+      .withMessage('`gender` 不可為空值')
+      .bail()
+      .isIn(['male', 'female', 'non-binary'])
+      .withMessage('`gender` 格式不正確'),
+
+    body('email')
+      .exists() // 欄位存在
+      .withMessage('欄位 `email` 必填')
+      .bail() // 不可為空
+      .notEmpty()
+      .withMessage('`email` 不可為空值')
+      .bail()
+      .isEmail()
+      .withMessage('`email` 格式錯誤'),
+
+    body('phone')
+      // 若有值才驗證
+      .if((phoneNumber) => phoneNumber)
+      .isMobilePhone('zh-TW')
+      .withMessage('`phone` 格式錯誤'),
+
+    body('agent')
+      .exists() // 欄位存在
+      .withMessage('欄位 `agent` 必填')
+      .bail()
+      .isMongoId() // 是否為 mongo id
+      .withMessage('無效的 `id`')
+      .bail() // id 不存在
+      .custom(async (id) => {
+        const matchItem = await agentsModel.findById(id)
+        if (!matchItem) throw new Error('`id` 不存在')
+      }),
+
+    body('password')
+      // 請混合使用 8 個字元以上的英文字母、數字和符號 (google rules)
+      .exists() // 欄位存在
+      .withMessage('欄位 `password` 必填')
+      .bail() // 不可為空
+      .notEmpty()
+      .withMessage('`password` 不可為空值')
+      .bail()
+      .isLength({ min: 8 })
+      .withMessage('密碼至少需要 8 個字元')
+      .matches(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)
+      .withMessage('密碼需要包含英文字母、數字和符號'),
+  ],
+
   deleteCustomer: [],
 }
 
@@ -9,7 +74,7 @@ const getCustomers = catchAsync(async (req, res, next) => {
   res.send('GET Customer')
 })
 const createCustomer = catchAsync(async (req, res, next) => {
-  res.send('POST Customer')
+  res.send(req.body)
 })
 const deleteCustomer = catchAsync(async (req, res, next) => {
   res.send('DELETE Customer')
