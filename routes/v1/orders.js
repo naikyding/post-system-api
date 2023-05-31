9f9773696b17decfce8f0ab726292018b711f241
Naiky <74ding@gmail.com>
2023-05-31 17:45:48 +0800

    feat(router): orders

--- routes/v1/orders.js
+++ routes/v1/orders.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+const validateHandler = require('../../utils/validateHandler')
+
 const {
+  validation,
+
   getOrderList,
   createOrder,
   deleteOrder,
 } = require('../../controllers/orders.controller')
 
 router.get('/', getOrderList)
-router.post('/', createOrder)
-router.delete('/:id', deleteOrder)
+router.post('/', validation.createOrder, validateHandler, createOrder)
+router.delete('/:id', validation.deleteOrder, validateHandler, deleteOrder)
 
 module.exports = router
