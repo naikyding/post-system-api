43b57e44f7df2565108e47bb205494ebfe3bf51c
Naiky <74ding@gmail.com>
2023-06-01 16:54:02 +0800

    feat(model): orders remove versionkey

--- models/orders.model.js
+++ models/orders.model.js
@@ -1,72 +1,78 @@
 const mongoose = require('mongoose')
 
-const ordersSchema = mongoose.Schema({
-  customer: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Customer',
-    required: [true, '欄位 `customer` 必填'],
-  },
+const ordersSchema = mongoose.Schema(
+  {
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Customer',
+      required: [true, '欄位 `customer` 必填'],
+    },
 
-  items: {
-    type: [
-      {
-        product: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'Product',
-          required: [true, '欄位 `items.product` 必填'],
-        },
-        extras: [
-          {
+    items: {
+      type: [
+        {
+          product: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Extra',
+            ref: 'Product',
+            required: [true, '欄位 `items.product` 必填'],
           },
-        ],
-        quantify: {
-          type: Number,
-          required: [true, '欄位 `items.quantify` 必填'],
+          extras: [
+            {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: 'Extra',
+            },
+          ],
+          quantify: {
+            type: Number,
+            required: [true, '欄位 `items.quantify` 必填'],
+          },
+          price: {
+            type: Number,
+            required: [true, '欄位 `items.price` 必填'],
+          },
         },
-        price: {
-          type: Number,
-          required: [true, '欄位 `items.price` 必填'],
-        },
-      },
-    ],
-    default: [],
-  },
+      ],
+      default: [],
+    },
 
-  totalPrice: {
-    type: Number,
-    required: [true, '欄位 `totalPrice` 必填'],
-  },
+    totalPrice: {
+      type: Number,
+      required: [true, '欄位 `totalPrice` 必填'],
+    },
 
-  isPaid: {
-    type: Boolean,
-    default: false,
-    // required: [true, '欄位 `isPaid` 必填'],
-  },
+    isPaid: {
+      type: Boolean,
+      default: false,
+      // required: [true, '欄位 `isPaid` 必填'],
+    },
 
-  status: {
-    type: String,
-    enum: [
-      'pending', // 待處理
-      'inProgress', // 進行中
-      'completed', // 完成
-      'cancelled', // 取消
-      'awaitingPayment', // 等待付款
-    ],
-    default: 'pending',
-  },
+    status: {
+      type: String,
+      enum: [
+        'pending', // 待處理
+        'inProgress', // 進行中
+        'completed', // 完成
+        'cancelled', // 取消
+        'awaitingPayment', // 等待付款
+      ],
+      default: 'pending',
+    },
 
-  note: {
-    type: String,
-    default: '',
-  },
+    note: {
+      type: String,
+      default: '',
+    },
 
-  agent: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Agent',
-    required: [true, '欄位 `agent` 必填'],
+    agent: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Agent',
+      required: [true, '欄位 `agent` 必填'],
+    },
   },
-})
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+)
 
 module.exports = mongoose.model('Order', ordersSchema)
