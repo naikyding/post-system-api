f61fb8cd107e0a993d9d57c69ae3d8e17bb35096
Naiky <74ding@gmail.com>
2023-06-08 17:47:13 +0800

    feat(controller): orders 新增欄位

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -189,12 +189,16 @@ const createOrder = catchAsync(async (req, res) => {
     else return (acc = [...acc, cur])
   }, [])
 
-  const { customer, totalPrice, agent } = req.body
+  const { customer, totalPrice, agent, note, isPaid, paymentType } = req.body
 
   const createdOrder = await ordersModel.create({
     customer,
     totalPrice,
     agent,
+    note,
+    isPaid,
+    paymentType,
+
     items: computedItemsData,
   })
 
