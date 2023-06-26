c33e338bdae47f94cf1afbc23d5a52c3f126ca39
Naiky <74ding@gmail.com>
2023-06-26 17:31:03 +0800

    feat(controller): users validation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,31 +4,77 @@ const validation = {
   adminLogin: [
     body('account')
       .exists() // 欄位存在
-      .withMessage('欄位 `account` 必填')
+      .withMessage('帳號 必填')
       .bail() // 不可為空
       .notEmpty()
-      .withMessage('`account` 不可為空值')
+      .withMessage('帳號 不可為空值')
       .bail()
       .isString() // 為字串格式
-      .withMessage('`account` 必須為字串格式'),
+      .withMessage('帳號 必須為字串格式'),
     body('password')
       .exists() // 欄位存在
-      .withMessage('欄位 `password` 必填')
+      .withMessage('密碼 必填')
       .bail() // 不可為空
       .notEmpty()
-      .withMessage('`password` 不可為空值')
+      .withMessage('密碼 不可為空值')
       .bail()
       .isString() // 為字串格式
-      .withMessage('`password` 必須為字串格式'),
+      .withMessage('密碼 必須為字串格式'),
+  ],
+
+  createUsers: [
+    body('email')
+      .exists() // 欄位存在
+      .withMessage('email 必填')
+      .bail() // 不可為空
+      .notEmpty()
+      .withMessage('email 不可為空值')
+      .bail()
+      .isEmail()
+      .withMessage('`email` 格式錯誤'),
+    body('password')
+      .exists() // 欄位存在
+      .withMessage('password 必填')
+      .bail() // 不可為空
+      .notEmpty()
+      .withMessage('password 不可為空值')
+      .bail()
+      .isLength({ min: 8 })
+      .withMessage('密碼至少需要 8 個字元')
+      .matches(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)
+      .withMessage('密碼需要包含英文字母、數字和符號 @$!%*?&'),
+    body('agents')
+      .exists() // 欄位存在
+      .withMessage('agents 必填')
+      .bail() // 不可為空
+      .notEmpty()
+      .withMessage('agents 不可為空值')
+      .bail()
+      .isMongoId() // 是否為 mongo id
+      .withMessage('無效的 agents `id`'),
+    body('roles')
+      .exists() // 欄位存在
+      .withMessage('roles 必填')
+      .bail() // 不可為空
+      .notEmpty()
+      .withMessage('roles 不可為空值')
+      .bail()
+      .isMongoId() // 是否為 mongo id
+      .withMessage('無效的 roles `id`'),
   ],
 }
 
-const adminLogin = (req, res) => {
+const userLogin = (req, res) => {
+  res.send(req.body)
+}
+
+const createUsers = (req, res) => {
   res.send(req.body)
 }
 
 module.exports = {
   validation,
 
-  adminLogin,
+  userLogin,
+  createUsers,
 }
