bec4dfbb991873639ded209e35b64656b78c9fe6
Naiky <74ding@gmail.com>
2023-06-26 17:30:48 +0800

    feat(model): users

---
+++ models/users.model.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      required: [true, '欄位 `email` 必填'],
+      type: String,
+    },
+    password: {
+      required: [true, '欄位 `password` 必填'],
+      type: String,
+    },
+    roles: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, 'agents 是必填項目'],
+        ref: 'Role',
+      },
+    ],
+    agents: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, 'agents 是必填項目'],
+        ref: 'Agent',
+      },
+    ],
+
+    nickname: {
+      type: String,
+      default: '',
+    },
+    avatar: {
+      type: String,
+      default: '',
+    },
+    phone: {
+      type: String,
+      default: '',
+    },
+    note: {
+      type: String,
+      default: '',
+    },
+
+    // Auth2
+    googleId: { type: String, default: '' },
+    appleId: { type: String, default: '' },
+    lineId: { type: String, default: '' },
+    facebook: { type: String, default: '' },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('User', userSchema)
