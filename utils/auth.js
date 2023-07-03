da52b9d0c9dd6615703889905098a5ce8a2edd3c
Naiky <74ding@gmail.com>
2023-07-03 17:49:39 +0800

    feat(utils): auth token 產生

--- utils/auth.js
+++ utils/auth.js
@@ -1,4 +1,5 @@
 const { errorResponse } = require('./responseHandlers')
+const jwt = require('jsonwebtoken')
 
 // (middleware) 驗證 token 是否夾帶於 header
 const auth = (req, res, next) => {
@@ -14,8 +15,12 @@ const auth = (req, res, next) => {
   next()
 }
 
-// 產生 JWT token
-const generatorToken = () => {}
+// 產生 JWT token (exp 效期)
+const generatorToken = (payload, exp = 120) => {
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: exp,
+  })
+}
 
 // 驗證 JWT token
 const verifyToken = () => {}
